276efef55ba6e9c989b90d1fc958627cc1240e1f
nhcodes <nhdotcodes@gmail.com>
2023-06-29 19:08:34 +0200

    improved and fixed draggable function

--- draggable.js
+++ draggable.js
@@ -1,25 +1,21 @@
-function makeDraggable(element) {
-    element.onmousedown = (downEvent) => {
+function makeDraggable(element, dragElement = element) {
+    dragElement.onmousedown = (downEvent) => {
 
-        const offsetX = downEvent.offsetX;
-        const offsetY = downEvent.offsetY;
-        /*element.style.left = (downEvent.clientX - offsetX) + "px";
-        element.style.top = (downEvent.clientY - offsetY) + "px";
-        element.classList.add("position-absolute");*/
-
+        const boundingClientRect = element.getBoundingClientRect();
+        const offsetX = downEvent.clientX - boundingClientRect.left;
+        const offsetY = downEvent.clientY - boundingClientRect.top;
 
         document.documentElement.onmousemove = (moveEvent) => {
             const newPositionX = moveEvent.clientX - offsetX;
             const newPositionY = moveEvent.clientY - offsetY;
-            element.style.left = newPositionX + "px";
-            element.style.top = newPositionY + "px";
-            element.classList.remove("top-50", "start-50", "translate-middle"); //todo
+            element.style.left = `${newPositionX}px`;
+            element.style.top = `${newPositionY}px`;
         };
 
         document.documentElement.onmouseup = (upEvent) => {
             document.documentElement.onmousemove = null;
             document.documentElement.onmouseup = null;
-            element.onmouseup = null;
+            dragElement.onmouseup = null;
         };
 
         downEvent.stopPropagation();
